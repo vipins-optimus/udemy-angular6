bc7927a68b7f8ae95b476a4f557c657891970e9a
vipins-optimus <vipin.sharma@optimusinfo.com>
2018-10-09 10:56:57 +0530

    L-53 Done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
 	selector: 'app-shopping-list',
 	templateUrl: './shopping-list.component.html',
 	styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-	recipes = [];
+	ingredients: Ingredient[] = [
+		new Ingredient('Apples', 5),
+		new Ingredient('Tomatoes', 15)
+	];
 
 	constructor() { }
 
