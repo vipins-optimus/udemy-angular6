5dd26e178fbf111816372bc2075a1f11daa1891b
vipin sharma <vipinsharma@vipins-MacBook-Air.local>
2019-03-31 12:54:22 +0530

    L-213 done

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -34,10 +34,26 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 			);
 	}
 
-	onAddItem(form: NgForm) {
+	onSubmit(form: NgForm) {
 		const value = form.value;
 		const newIngredient = new Ingredient(value.name, value.amount);
-		this.shoppingListService.addIngredient(newIngredient);
+		if (this.editMode) {
+			this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+		} else {
+			this.shoppingListService.addIngredient(newIngredient);
+		}
+		this.editMode = false;
+		this.slForm.reset();
+	}
+
+	clearForm() {
+		this.slForm.reset();
+		this.editMode = false;
+	}
+
+	deleteIngredient() {
+		this.shoppingListService.deleteIngredient(this.editedItemIndex);
+		this.clearForm();
 	}
 
 	ngOnDestroy() {
